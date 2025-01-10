07fd3d2b99116673ca90195c1641d37bf38f9a79
kckurkcu1 <100448747+kckurkcu1@users.noreply.github.com>
2025-01-10 17:19:17 +0300

    x

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
-import "os"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kckurkcu1/GO-Restaurant-Management/database"
+	"github.com/kckurkcu1/GO-Restaurant-Management/middleware"
+	"github.com/kckurkcu1/GO-Restaurant-Management/routes"
+	"go.mongodb.org/mongo-driver/mongo"
+	"os"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		port = "8080"
+		port = "8000"
 	}
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
 }
--- routes/foodRouter.go
+++ routes/foodRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	controller "github.com/kckurkcu1/GO-Restaurant-Management/controllers"
+)
+
+func FoodRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/food", controller.GetFoods())
+	incomingRoutes.GET("/foods/:food_id", controller.GetFood())
+	incomingRoutes.POST("/foods", controller.CreateFood())
+	incomingRoutes.PATCH("/foods/:food_id", controller.UpdateFood())
+}
